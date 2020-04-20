a4770c9caef683bc77d6dcf084ca94ddd60bf4f5
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-04-20 09:04:04 +0200

    More conversion improvements.

--- src/BinaryReader.ts
+++ src/BinaryReader.ts
@@ -1,24 +1,27 @@
 export default class BinaryReader {
-	constructor(arrayBuffer) {
+	array: Uint8Array
+	position: number
+
+	constructor(arrayBuffer: ArrayBuffer) {
 		this.array = new Uint8Array(arrayBuffer)
 		this.position = 0
 	}
-	read() {
+	read(): number {
 		return this.array[this.position++]
 	}
-	readBytes(size) {
+	readBytes(size: number): Uint8Array {
 		const start = this.position
 		const end = this.position + size
 		this.position = end
 		return this.array.slice(start, end)
 	}
-	readUInt32() {
+	readUInt32(): number {
 		const newPosition = this.position + 4
 		const byte = this.array.slice(this.position, newPosition)
 		this.position = newPosition
 		return byte.reduce((p, c, i) => c << (i * 8) | p)
 	}
-	readString(size) {
+	readString(size: number): string {
 		return String.fromCharCode.apply(String, this.readBytes(size))
 	}
 }
