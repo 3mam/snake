1a9f87ffd648068001b1c7a1badddcd9d78ce46d
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-04-28 18:38:09 +0200

    Rename position to offset.

--- src/BinaryReader.ts
+++ src/BinaryReader.ts
@@ -1,25 +1,25 @@
 export default class BinaryReader {
 	array: Uint8Array
-	position: number
+	offset: number
 
 	constructor(array: ArrayBuffer | Uint8Array) {
 		array instanceof ArrayBuffer && (this.array = new Uint8Array(array))
 		array instanceof Uint8Array && (this.array = array)
-		this.position = 0
+		this.offset = 0
 	}
 	read(): number {
-		return this.array[this.position++]
+		return this.array[this.offset++]
 	}
 	readBytes(size: number): Uint8Array {
-		const start = this.position
-		const end = this.position + size
-		this.position = end
+		const start = this.offset
+		const end = this.offset + size
+		this.offset = end
 		return this.array.slice(start, end)
 	}
 	readUInt32(): number {
-		const newPosition = this.position + 4
-		const byte = this.array.slice(this.position, newPosition)
-		this.position = newPosition
+		const newPosition = this.offset + 4
+		const byte = this.array.slice(this.offset, newPosition)
+		this.offset = newPosition
 		return byte.reduce((p, c, i) => c << (i * 8) | p)
 	}
 	readString(size: number): string {
