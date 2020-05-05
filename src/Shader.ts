26cd348721b5e203e5cf897338de0567f3e319ed
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-05-06 01:57:14 +0200

    Improvement and polish.

--- src/Shader.ts
+++ src/Shader.ts
@@ -23,36 +23,25 @@ function createProgram(gl: WebGL2RenderingContext, vertexShader: WebGLShader, fr
 	gl.deleteProgram(program)
 }
 
-export enum VarType {
-	Int,
-	Float,
-	Vec2,
-	Vec3,
-	Vec4,
-	Mat3,
-	Mat4,
-}
-
-enum LocationType {
+enum Type {
 	Vertex,
 	Uniform,
 	Texture,
-}
-
-type Variable = {
-	value: any
-	type: VarType
+	UV,
+	Mat4,
+	Float,
+	Vec4,
 }
 
 type ShaderVar = {
-	handle?: number | WebGLUniformLocation
-	type: LocationType
-	variable: Variable
+	handle: number | WebGLUniformLocation
+	type: Type
+	variable: any
 }
 
 export class Shader {
-	program: WebGLProgram
-	varList: Map<string, ShaderVar>
+	private program: WebGLProgram
+	private varList: Map<string, ShaderVar>
 	private gl: WebGL2RenderingContext
 
 	constructor(gl: WebGL2RenderingContext) {
@@ -79,48 +68,67 @@ export class Shader {
 			outColor = texture(TEXTURE, UV)*COLOR;
 		}
 		`
+
 		const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
 		const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
 		this.program = createProgram(gl, vertexShader, fragmentShader)
 		this.varList = new Map()
-		this.varList.set('VERTEX', { type: LocationType.Vertex, variable: { value: null, type: VarType.Vec3 } })
-		this.varList.set('inUV', { type: LocationType.Vertex, variable: { value: null, type: VarType.Vec2 } })
-		this.varList.set('MATRIX', { type: LocationType.Uniform, variable: { value: null, type: VarType.Mat4 } })
-		this.varList.set('TEXTURE', { type: LocationType.Texture, variable: { value: null, type: VarType.Vec2 } })
-		this.varList.set('COLOR', { type: LocationType.Uniform, variable: { value: null, type: VarType.Vec4 } })
+		this.varList.set('VERTEX', { type: Type.Vertex, handle: null, variable: null })
+		this.varList.set('inUV', { type: Type.UV, handle: null, variable: null })
+		this.varList.set('MATRIX', { type: Type.Mat4, handle: null, variable: null })
+		this.varList.set('TEXTURE', { type: Type.Texture, handle: null, variable: null })
+		this.varList.set('COLOR', { type: Type.Vec4, handle: null, variable: null })
 
 		this.varList.forEach((v, k) => {
-			v.type === LocationType.Vertex && (v.handle = this.gl.getAttribLocation(this.program, k))
-			v.type === LocationType.Uniform && (v.handle = this.gl.getUniformLocation(this.program, k))
-			v.type === LocationType.Texture && (v.handle = this.gl.getUniformLocation(this.program, k))
+			switch (v.type) {
+				case Type.Vertex:
+				case Type.UV:
+					v.handle = this.gl.getAttribLocation(this.program, k)
+					break
+				case Type.Texture:
+				case Type.Mat4:
+				case Type.Vec4:
+				case Type.Float:
+					v.handle = this.gl.getUniformLocation(this.program, k)
+					break
+			}
 		})
 	}
-	update() {
+
+	set(name: string, variable: any): void {
+		this.varList.get(name).variable = variable
+	}
+
+	get(name: string): any {
+		return this.varList.get(name).variable
+	}
+
+	update(): void {
 		this.gl.useProgram(this.program)
-		this.varList.forEach((v, k) => {
+		this.varList.forEach((v) => {
 			switch (v.type) {
-				case LocationType.Vertex:
-					this.gl.bindBuffer(this.gl.ARRAY_BUFFER, v.variable.value)
+				case Type.Vertex:
+					this.gl.bindBuffer(this.gl.ARRAY_BUFFER, v.variable)
 					this.gl.enableVertexAttribArray(<number>v.handle)
-					this.gl.vertexAttribPointer(<number>v.handle, v.variable.type === VarType.Vec2 ? 2 : 3, this.gl.FLOAT, false, 0, 0)
+					this.gl.vertexAttribPointer(<number>v.handle, 3, this.gl.FLOAT, false, 0, 0)
 					break
-				case LocationType.Texture:
-					this.gl.bindTexture(this.gl.TEXTURE_2D, v.variable.value)
+				case Type.UV:
+					this.gl.bindBuffer(this.gl.ARRAY_BUFFER, v.variable)
+					this.gl.enableVertexAttribArray(<number>v.handle)
+					this.gl.vertexAttribPointer(<number>v.handle, 2, this.gl.FLOAT, false, 0, 0)
+					break
+				case Type.Texture:
+					this.gl.bindTexture(this.gl.TEXTURE_2D, v.variable)
 					this.gl.uniform1i(v.handle, 0)
 					break
-				case LocationType.Uniform:
-					switch (v.variable.type) {
-						case VarType.Mat4:
-							this.gl.uniformMatrix4fv(v.handle, false, v.variable.value)
-							break
-						case VarType.Vec4:
-							this.gl.uniform4fv(v.handle, v.variable.value)
-							break
-						default:
-							break
-					}
+				case Type.Mat4:
+					this.gl.uniformMatrix4fv(v.handle, false, v.variable)
 					break
-				default:
+				case Type.Vec4:
+					this.gl.uniform4fv(v.handle, v.variable)
+					break
+				case Type.Float:
+					this.gl.uniform1f(v.handle, v.variable)
 					break
 			}
 		})
