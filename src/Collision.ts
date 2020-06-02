8decb7557a549744b3dcb969ac58a73dbabe271e
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-06-02 13:55:09 +0200

    Add circle to circle collision.

---
+++ src/Collision.ts
@@ -0,0 +1,28 @@
+import { Vec3 } from './Math'
+
+export class Circle {
+	x: number
+	y: number
+	r: number
+
+	constructor(x: number, y: number, r: number) {
+		this.x = x
+		this.y = y
+		this.r = r
+	}
+
+	collisionTo(c: Circle): boolean {
+		const dx = this.x - c.x
+		const dy = this.y - c.y
+		const distance = Math.sqrt(dx * dx + dy * dy)
+
+		if (distance < this.r + c.r) {
+			return true
+		}
+		return false
+	}
+
+	moveTo(v: Vec3): Circle {
+		return new Circle(v.x, v.y, this.r)
+	}
+}
