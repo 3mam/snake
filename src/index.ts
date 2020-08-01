b3934ddd47b98c6a92f747e374c7513accee9b90
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-08-01 22:39:47 +0200

    Fix graphical glitch.

--- src/index.ts
+++ src/index.ts
@@ -75,6 +75,14 @@ class Vec2in1D {
 	}
 }
 
+function dpadVisibleSwitch() {
+	let dpad = document.getElementById('dpad').style
+	if (dpad.visibility === 'visible')
+		dpad.visibility = 'hidden'
+	else
+		dpad.visibility = 'visible'
+}
+
 window.onload = () => {
 
 	class Game extends Engine {
@@ -106,18 +114,18 @@ window.onload = () => {
 			const toggleFullScreen = () => {
 				if (!document.fullscreenElement) {
 					document.documentElement.requestFullscreen()
-					document.getElementById('dpad').style.visibility = 'visible'
 				} else {
 					if (document.exitFullscreen) {
 						document.exitFullscreen()
-						document.getElementById('dpad').style.visibility = 'hidden'
-
 					}
 				}
 			}
 
+			window.addEventListener('fullscreenchange', (ev) => {
+				dpadVisibleSwitch()
+			})
+
 			window.addEventListener('orientationchange', (ev) => {
-				//console.log(ev.target['screen']['orientation']['angle'])
 				this.screenAngle = ev.target['screen']['orientation']['angle']
 			})
 
@@ -169,45 +177,30 @@ window.onload = () => {
 
 			})
 
-
-			const model = await loadNode('./data/snake.gltf')
+			const model = await loadNode('./data/snake.glb')
 			this.arena = model.get('arena')
-
 			this.cam = new Camera('main')
 			this.cam.useAsCurrent()
 
-			const area = new Array(3)
+			//const area = new Array(3)
 			this.arenaSpawnPoint = new Array(width * height)
 			for (let y = 0; y < height; y++) {
 				for (let x = 0; x < width; x++) {
 					const mat4 = new Mat4()
-					let wall = -0.05
-					if (width - 1 === x && height - 1 === y) {
-						mat4.translate(new Vec3(x * 0.06, y * 0.06, wall + 0.10))
-						area[1] = mat4
-					} else if (x === 0 && y === 0) {
-						mat4.translate(new Vec3(x * 0.06, y * 0.06, wall + 0.10))
-						area[0] = mat4
-					} else {
-						mat4.translate(new Vec3((width / 2) * 0.06, (height / 2) * 0.06, wall))
-						mat4.scaleX(width / 3.3)
-						mat4.scaleY(height / 3.3)
-
-						area[2] = mat4
-					}
 					this.pos2D.x = x
 					this.pos2D.y = y
 					this.arenaSpawnPoint[this.pos2D.position] = new Vec3(x * 0.06, y * 0.06, 0.0005)
 				}
 			}
-			this.arena.createInstance(area)
+			//this.arena.createInstance(area)
+			this.arena.translate(new Vec3((width / 2) * 0.06, (height / 2) * 0.06, 0))
 			this.speed = 0.9
 
 			this.pos2D.x = width / 2
 			this.pos2D.y = 0
 
-			let pos = this.arenaSpawnPoint[this.pos2D.position].copy()
-			pos = pos.add(new Vec3(0, 0, 0.05))
+			let pos = this.arenaSpawnPoint[this.pos2D.position]
+			pos = pos.add(new Vec3(0, 0, 0))
 			this.direction = EDirection.up
 			const snakeLength = 50
 			this.midleSnakeInstance = new Array(snakeLength)
@@ -217,8 +210,9 @@ window.onload = () => {
 
 			for (let i = 0; i < snakeLength; i++) {
 				this.midleSnakeInstance[i] = new Mat4
-				this.midleSnakeNode[i] = new GameObject(midle, pos, i == 0 ? true : false, i == 0 ? this.head : this.midleSnakeNode[i - 1], i)
+				this.midleSnakeInstance[i].scale(new Vec3(0, 0, 0))
 
+				this.midleSnakeNode[i] = new GameObject(midle, pos, i == 0 ? true : false, i == 0 ? this.head : this.midleSnakeNode[i - 1], i)
 			}
 			midle.createInstance(this.midleSnakeInstance)
 
@@ -226,19 +220,19 @@ window.onload = () => {
 			this.tail = new GameObject(model.get('tail'), pos, true, this.midleSnakeNode[0])
 			const random = getRandomInt(0, this.arenaSpawnPoint.length)
 			this.up = new GameObject(model.get('up'), new Vec3(this.arenaSpawnPoint[random].valueX(), this.arenaSpawnPoint[random].valueY()))
-			this.up.resize(new Vec3(0.05, 0.05, 0.05))
 			this.snakeSize = 1
 		}
 
 		camera(zoom: number) {
-			this.cam.position(new Vec3(0, 2, -2).subtract(this.head.currentPosition().multiply(zoom)))
+			this.cam.position(new Vec3(0, 2, -2).subtract(this.head.currentPosition().multiply(new Vec3(zoom, zoom, zoom))))
 			this.cam.zoom(zoom)
 		}
 
 		update(delta) {
 			this.cam.identity()
 			this.cam.rotateX(0.9)
-			this.screenAngle != 0 ? this.camera(2) : this.camera(1)
+
+			this.screenAngle != 0 ? this.camera(1.5) : this.camera(1)
 
 			this.head.lookDirection(this.direction)
 			this.head.move(this.speed * delta)
@@ -250,7 +244,7 @@ window.onload = () => {
 				m.rotateAt(parentLastState.direction)
 				this.snakeSize > i ? m.show() : m.hide()
 				m.update()
-				if (this.head.collisionWithObject(m, 0.02)) {
+				if (this.head.collisionWithObject(m, 0.03)) {
 					this.speed = 0
 				}
 			})
@@ -278,7 +272,6 @@ window.onload = () => {
 			this.head.render()
 			this.midleSnakeNode[0].render()
 			this.tail.render()
-
 		}
 	}
 
